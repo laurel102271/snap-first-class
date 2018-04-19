5679d47fa0c5f14d2201b666b3379dfbde15834c
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-19 08:38:29 -0600

    PHP Snap Challenge

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$mysmallnumber =12;
+$myTotal =356;
+$myTotal = $mysmall  Number + $myLargeNumber;
+echo $myTotal;
+?>
+/**
+ * Created by PhpStorm.
+ * User: Daniel Martinez
+ * Date: 4/19/2018
+ * Time: 8:18 AM
+ */
